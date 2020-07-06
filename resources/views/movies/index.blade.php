52947c4f9ff3916ad5659aedf2d04f7be1ee5b1a
khantzawhein <kzh702@gmail.com>
2020-07-06 15:18:37 +0000

    nav-bar bug fixed

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,7 +5,7 @@
         <h4 class="text-uppercase text-warning font-weight-bold mt-2">Popular Movies</h4>
 
         <div class="row mt-2">
-            
+
             @foreach ($popularMovies as $movie)
                 <x-movie-card :movie="$movie" :genres="$genres"/>
             @endforeach
@@ -13,16 +13,16 @@
 
         </div>
 
-        
+
     </div>
 
     <div class="container border-bottom border-dark py-4">
         <h4 class="text-uppercase text-warning font-weight-bold mt-2">Now Playing</h4>
 
         <div class="row mt-2">
-            
+
             @foreach ($nowPlayingMovies as $movie)
-                
+
                 <x-movie-card :movie="$movie" :genres="$genres"/>
 
             @endforeach
@@ -30,6 +30,6 @@
 
         </div>
 
-        
+
     </div>
 @endsection
