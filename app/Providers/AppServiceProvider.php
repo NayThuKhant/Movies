e7083d76dafc535144e79e993d83018f1cc30bc1
vagrant <vagrant@homestead>
2020-10-12 07:31:04 +0000

    final status

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      *
+     * @param UrlGenerator $url
      * @return void
      */
     public function boot(UrlGenerator $url)
